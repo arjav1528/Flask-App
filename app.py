f1315e0f379e90df23197e40b023473cdbec1983
Arjav Patel <arjav1528@gmail.com>
2025-02-13 03:35:23 +0530

    Hellllllllo

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
-@app.route('/hello'):
+@app.route('/hello')
 def some_function():
     return render_template('index.html')
 
